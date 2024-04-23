4894ee3297a4e61dddfc6deb5f274dd2dfb57dd3
El ayoubi Amine <aminetmtc@hotmail.com>
2024-04-23 14:08:22 +0100

    TP3 part 2

--- TP3/src/main/java/ma/emsi/tp3/Tp3Application.java
+++ TP3/src/main/java/ma/emsi/tp3/Tp3Application.java
@@ -21,9 +21,9 @@ public class Tp3Application implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        patientRepository.save(new Patient(null,"Mohamed",new Date(),false,34));
+        patientRepository.save(new Patient(null,"Mohamed",new Date(),false,314));
         patientRepository.save(new Patient(null,"Hanane",new Date(),false,4321));
-        patientRepository.save(new Patient(null,"Imane",new Date(),true,34));
+        patientRepository.save(new Patient(null,"Imane",new Date(),true,324));
 
     }
 }
